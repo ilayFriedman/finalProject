f729d06c1c1233d59afcd333ed2b160d4b2ee909
Oren Shor <shororen@post.bgu.ac.il>
2020-03-30 18:12:56 +0300

    oren

--- Application/Frontend/src/app/mapsfolders-viewer/mapsfolders-viewer.component.ts
+++ Application/Frontend/src/app/mapsfolders-viewer/mapsfolders-viewer.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FolderHandlerService } from '../services/folder-handler.service';
 import { HttpClient } from '@angular/common/http';
 import { MapsHandlerService } from '../services/maps-handler.service';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button'
 
 const is = (fileName: string, ext: string) => new RegExp(`.${ext}\$`).test(fileName);
 @Component({
@@ -11,61 +12,25 @@ const is = (fileName: string, ext: string) => new RegExp(`.${ext}\$`).test(fileN
   styleUrls: ['./mapsfolders-viewer.component.css']
 })
 export class MapsfoldersViewerComponent implements OnInit {
-  
+
   localUrl = 'http://localhost:3000';
   // maps variables
   myMaps: any;
 
   //folders variables
-  currentLocation : String = "./(Root Folder)";
-  currntFolderID : any = null;
-  public expandedKeys: any[] = ['0', '1'];
-  public hasChildren = (item: any) => item.items && item.items.length > 0;
-  public fetchChildren = (item: any) => of(item.items);
-  public selectedKeys: any[] = ['0_1'];
-
-  public isItemSelected = (_: any, index: string) => this.selectedKeys.indexOf(index) > -1;
-
-  public handleSelection({ index }: any): void {
-    console.log("hi!!")
-      this.selectedKeys = [index];
-  }
-
-
   public data: any[] = [{
-    text: this.currentLocation,
+    text: "./",
     items: [
-        // {
-        //     text: 'firstFolder',
-        //     items: [
-        //         { text: 'map1' },
-        //         { text: 'map2' },
-        //         { text: 'map3' }
-        //     ]
-        // },
-        // { text: 'map5' }
-        // {
-        //     text: 'New Web Site',
-        //     items: [
-        //         { text: 'mockup.jpg' },
-        //         { text: 'Research.pdf' }
-        //     ]
-        // },
-        // {
-        //     text: 'Reports',
-        //     items: [
-        //         { text: 'February.pdf' },
-        //         { text: 'March.pdf' },
-        //         { text: 'April.pdf' }
-        //     ]
-        // }
-    ]
-}];
+      { text: "parent", items: [{ text: "child1", isFolder: false }], isFolder: true }
+
+    ],
+    isFolder: true
+  }];
 
   constructor(private folderHandler: FolderHandlerService, private mapHandler: MapsHandlerService, private http: HttpClient) {
-  } 
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     //maps init
     console.log("strat index")
     this.mapHandler.myMapsPromise.then(res => {
@@ -74,28 +39,29 @@ export class MapsfoldersViewerComponent implements OnInit {
       this.myMaps = res
       console.log('OK');
       // console.log("from index: " + this.myMaps);
-      
+
     }).catch
-      (err=> {
+      (err => {
         console.log("error here");
         console.log(err)
       })
 
-  
 
-  // folders init : find the rootFolder
 
-  
+    // folders init : find the rootFolder
+
+
     this.folderHandler.getRootUserFolder().then(res => {
-      
+
       console.log('======folder request=====');
       console.log(res)
       console.log('=================')
-      // this.insertMaps(res[0],null)
 
-      
+      this.inserMapsToMapTreeViewer(Object(res), null)
+
+
     }).catch
-      (err=> {
+      (err => {
         console.log("error here");
         console.log(err)
       })
@@ -103,42 +69,43 @@ export class MapsfoldersViewerComponent implements OnInit {
 
   }
 
-public iconClass({ text, items }: any): any {
-  return {
+  public iconClass({ text, items }: any): any {
+    return {
       'k-i-file-pdf': is(text, 'pdf'),
       'k-i-folder': items !== undefined,
       'k-i-html': is(text, 'html'),
       'k-i-image': is(text, 'jpg|png'),
       'k-icon': true
-  };
-}
+    };
+  }
 
-insertMaps(mapsIdsList,destinationFolder){
-  mapsIdsList.forEach(mapId => {
-    this.mapHandler.getMap(mapId).then(res => {
-     
-      
-      this.data.push({text: res.MapName})
-    }).catch
-      (err=> {
-        console.log("error with GetMap from insertMaps function");
-        console.log(err)
-      })
-  });
-}
+  inserMapsToMapTreeViewer(mapsIdsList, destinationFolder) {
+    console.log(mapsIdsList.MapsInFolder)
+    mapsIdsList.MapsInFolder.forEach(map => {
+      console.log(map);
 
-play(){
-  this.mapHandler.createMap("newMap","NEW dESC",{}).then(res => {
-    console.log('======CREATE MAP request=====');
-    console.log(res)
-    
-  }).catch
-    (err=> {
-      console.log("error here");
-      console.log(err)
-    })
+      this.data[0].items.push({ text: map.mapName, mapID: map.mapID, isFolder: false })
+    });
 
-    
-}
-}
+    console.log(this.data)
+  }
 
+  folderModal() {
+    // this.mapHandler.createMap("newMap","NEW dESC",{}).then(res => {
+    //   console.log('======CREATE MAP request=====');
+    //   console.log(res)
+
+    // }).catch
+    //   (err=> {
+    //     console.log("error here");
+    //     console.log(err)
+    //   })
+
+  }
+
+  addNewFolder(folderName, desc) {
+
+  }
+
+
+}
