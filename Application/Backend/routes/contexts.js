0c092a16c8ec8915ad3a2e7d4355ab568c4f3769
saarzeev <saarzeev@post.bgu.ac.il>
2020-05-13 15:20:18 +0300

    Context error handling and tests

--- Application/Backend/routes/contexts.js
+++ Application/Backend/routes/contexts.js
@@ -20,24 +20,28 @@ router.get('/private/getAllContexts', async function (req, res) {
 
 //TODO
 router.post('/private/createContext', async function (req, res) {
-    try {
-        const CreatorId = req.decoded._id;
-        const newCtx = new context({
-            Title: req.body.Title,
-            CreatorId: CreatorId,
-            CreationTime: new Date()
-        });
-        newCtx.save(function (err) {
-            if (err) {
-                console.log(err);
-                res.status(500).send(`Server error occured.`);
-            } else {
-                res.status(200).send('Context added successfully');
-            }
-        });
-    } catch (e) {
-        console.log(e);
-        res.status(500).send();
+    if(!req.body.Title){
+        res.status(400).send('Cannot create a Context without a Title field.');
+    }else{
+        try {
+            const CreatorId = req.decoded._id;
+            const newCtx = new context({
+                Title: req.body.Title,
+                CreatorId: CreatorId,
+                CreationTime: new Date()
+            });
+            newCtx.save(function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(`Server error occured.`);
+                } else {
+                    res.status(200).send('Context added successfully');
+                }
+            });
+        } catch (e) {
+            console.log(e);
+            res.status(500).send();
+        }
     }
 })
 
